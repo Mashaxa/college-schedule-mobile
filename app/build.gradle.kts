f030ce025ba0f221491bc20e78b03ef91cb532a8
Mashaxa <riaswaag@gmail.com>
2026-02-02 19:12:05 +0700

    Подключила необходимые библиотеки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,12 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
+    implementation("com.squareup.retrofit2:retrofit:2.12.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.12.0")
+    implementation("androidx.lifecycle:lifecycle-viemodel-compose:2.8.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
